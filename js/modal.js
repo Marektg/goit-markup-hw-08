a3b1d5875d8379404fd3909ffe26b3a17f6d3b9c
Marek Kalus <marektg2@gmail.com>
2022-03-06 21:02:59 +0100

    create mobile menu

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  const refs = {
+    openMobileMenulBtn: document.querySelector('[data-mobileMenu-open]'),
+    closeMobileMenuBtn: document.querySelector('[data-mobileMenu-close]'),
+    mobileMenu: document.querySelector('[data-mobileMenu]'),
+  };
+
+  refs.openMobileMenulBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileMenuBtn.addEventListener('click', toggleMobile);
+
+  function toggleMobile() {
+    refs.mobileMenu.classList.toggle('is-hidden');
+  }
+})();
