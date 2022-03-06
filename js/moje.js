3398c6f1edfc9a6c7810c71610d2844f88ff94a9
Marek Kalus <marektg2@gmail.com>
2022-03-06 18:36:10 +0100

    add mobile menu

--- js/moje.js
+++ js/moje.js
@@ -1,21 +1,13 @@
-var screenWidth = screen.width;
-console.log(screenWidth);
-var whatwedo = document.getElementById('whatwedo');
-var menu = document.getElementById('header__menu__navi');
-var mobile = document.getElementById('header__menu__navi-mobile');
-window.onload = checkScreen();
-function checkScreen() {
-    if (screenWidth < 1200) {
-        
-        whatwedo.setAttribute('display', 'none');
-        whatwedo.classList.toggle('is-hidden');
-      
-        menu.classList.toggle('is-hidden');
-    }
-    else {
-        whatwedo.classList.toggle('is-hidden');
+var menuBg = document.getElementById('header__menu__navi-mobile');
+var closeBnt = document.getElementById('mobile__closeBnt');
 
-        menu.classList.toggle('is-hidden');
-        mobile.classList.toggle('is-hidden');
-    }
+
+function showMobileMenu() {
+   
+    menuBg.setAttribute('class', 'mobile__menu-show');
+   
+    closeBnt.setAttribute('onclick', 'closeMobileMenu()');
+}
+function closeMobileMenu() {
+    menuBg.setAttribute('class', 'mobile__menu-is-hidden');
 }
