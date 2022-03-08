aeddcfe40b21fb9a26e67f1017e3d63342f7efba
Marek Kalus <marektg2@gmail.com>
2022-03-08 19:56:38 +0100

    update

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,26 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    modalWindow: document.querySelector('[data-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', widhtScreen);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', widhtScreen);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+  function widhtScreen() {
+    var szerokosc = screen.width;
+    
+    if (szerokosc < 1200) {
+      refs.modalWindow.classList.toggle('mobile');
+    }
+    else {
+      refs.modalWindow.classList.toggle('desktop');
+    }
   }
 })();
 (() => {
@@ -20,9 +33,14 @@
   };
 
   refs.openMobileMenulBtn.addEventListener('click', toggleMobile);
+ 
   refs.closeMobileMenuBtn.addEventListener('click', toggleMobile);
 
   function toggleMobile() {
     refs.mobileMenu.classList.toggle('is-hidden');
   }
 })();
+
+
+
+
