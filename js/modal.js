be50129dbe5f265d70fe7d25a54fb35f9019fccc
Marek Kalus <marektg2@gmail.com>
2022-03-09 11:36:05 +0100

    update description aboutus and whatwedo section to mobile resolution

--- js/modal.js
+++ js/modal.js
@@ -3,29 +3,15 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    modalWindow: document.querySelector('[data-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openModalBtn.addEventListener('click', widhtScreen);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', widhtScreen);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
-  function widhtScreen() {
-    var szerokosc = screen.width;
-    if (szerokosc > 320 , szerokosc < 480) {
-      refs.modalWindow.classList.toggle('small');
-    }
-    if (szerokosc < 1200) {
-      refs.modalWindow.classList.toggle('mobile');
-    }
-    else {
-      refs.modalWindow.classList.toggle('desktop');
-    }
-  }
+
 })();
 (() => {
   const refs = {
